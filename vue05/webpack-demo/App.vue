14e83171fd419c7be97ab11fa3cf5a4ccd5e0fe1
王威人 <291762380@qq.com>
2019-03-24 23:59:29 +0800

    单文件组件

---
+++ vue05/webpack-demo/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="itany">
+        <h1>welcome to 计科一班</h1>
+        <h2 @click="change">{{name}}</h2>
+
+        <!--使用User组件-->
+        <User></User>
+    </div>
+</template>
+
+<script>
+    //console.log(111);
+    //导入User.vue模块
+    import User from './components/User.vue'
+
+    export default {
+        data(){
+            return {
+                name:'tom'
+            }
+        },
+        methods : {
+            change(){
+                this.name = '汤姆'
+            }
+        },
+        //引入组件
+        components : {
+            User  //简写，User:User  'User':{template:User}
+        }
+    }
+</script>
+
+<style>
+    body{
+        background-color:#ccc;
+    }
+</style>
